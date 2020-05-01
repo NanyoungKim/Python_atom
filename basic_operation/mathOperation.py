4000b9d3d2cae49bdab5834b857129ce06eff3ab
NanyoungKim <nanyoung18@gmail.com>
2020-05-01 18:50:38 +0900

    math opeartion : ceil and floor

---
+++ basic_operation/mathOperation.py
@@ -0,0 +1,9 @@
+import math 
+
+
+print(math.ceil(2.2))     #올림 -> 3 출력
+
+print(math.floor(2.7))    #내림 -> 2 출력
+
+print(math.pow(2,10))     #2의 10제곱 -> 1024.0 출력
+print(math.pi)           #3.141592653589793 출력
