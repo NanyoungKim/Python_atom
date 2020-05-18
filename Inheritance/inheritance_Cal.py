d948c48f6fb2f6a80fe1797cc6f438d7ca77651d
NanyoungKim <nanyoung18@gmail.com>
2020-05-18 13:41:53 +0900

    계산기를 이용한 상속 이해

--- Inheritance/inheritance_Cal.py
+++ Inheritance/inheritance_Cal.py
@@ -1,20 +1,34 @@
-class Calculator(object):
-    def __init__(self, v1, v2):    #self의 기능은? 파이썬에서는 첫번째 매개변수를 꼭 정의해야한다. 첫번째 매개변수는 언제나 그 인스턴스를 가리킨다.
-        print(v1,v2)
-        self.v1 = v1
-        self.v2 = v2
-
+class Cal(object):
+    def __init__(self, v1, v2):
+        if isinstance(v1, int):
+            self.v1 = v1
+        if isinstance(v2, int):
+            self.v2 = v2
     def add(self):
         return self.v1+self.v2
-
     def subtract(self):
         return self.v1-self.v2
+    def setV1(self, v):
+        if isinstance(v, int):
+            self.v1 = v
+    def getV1(self):
+        return self.v1
+
+class CalMultiply(Cal):
+    def multiply(self):
+        return self.v1*self.v2
+
+class CalDivide(CalMultiply):
+    def divide(self):
+        return self.v1/self.v2
 
 
-c1 = Calculator(10,10)
+c1 = CalMultiply(10,10)
 print(c1.add())
-print(c1.subtract())
+print(c1.multiply())
 
-c2 = Calculator(30,20)
-print(c2.add())
-print(c2.subtract())
+
+c2 = CalDivide(20,10)
+print(c2, c2.add())
+print(c2, c2.multiply())
+print(c2, c2.divide())
